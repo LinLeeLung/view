4d4277f72bb6cd3fa31b0f81baedf64cc983f6e0
Daniel <31122395+LinLeeLung@users.noreply.github.com>
2025-04-13 14:06:05 +0800

    調整手機顯示+deploy設定

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import postcss from '@tailwindcss/postcss';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/accn/' , // ✅這裡要設定正確 repo 名
   css: {
     postcss: {
       plugins: [postcss()],
